c216c9519ae95c1ed3ba8315b9e3e447ce9fea5f
Ho Xuan Vinh <xuan-vinh.ho@etu.utc.fr>
2021-06-16 14:43:09 +0200

    fix file1 in self

--- file1.py
+++ file1.py
@@ -8,7 +8,6 @@ PROJET SY31 P21: HO Xuan Vinh
 import numpy as np
 import rospy
 from geometry_msgs.msg import PoseStamped
-from turtlebot3_msgs.msg import SensorState
 from std_msgs.msg import Int32, Float32
 from geometry_msgs.msg import Twist, Point32
 from sensor_msgs.msg import LaserScan, PointCloud2, PointField
@@ -16,14 +15,7 @@ from sensor_msgs.msg import Imu, MagneticField
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
 
 #Init valeurs
-global d_forward = None
-global d_right = None
-global d_left = None
-global d_back = None
 
-global distance_obstacle = 0.1
-global obstacles = []
-global coords = []
 
 PC2FIELDS = [PointField('x', 0, PointField.FLOAT32, 1),
              PointField('y', 4, PointField.FLOAT32, 1),
@@ -45,15 +37,22 @@ class moving:
         self.target_surface = Float32()
         self.target_surface = 0
 
+        self.distance_obstacle = 0.1
+
+        self.d_forward = 0
+        self.d_left = 0
+        self.d_right = 0
+        self.d_back=0
+
     def move(self,msg):
         msg_cmd_vel = Twist()
 
         print('Surface target: ', self.target_surface,'\n')
 
-        distance_min = min(d_back,d_forward,d_left,d_right)
-        distance_max = max(d_back,d_forward,d_left,d_right)
+        distance_min = min(self.d_back,self.d_forward,self.d_left,self.d_right)
+        distance_max = max(self.d_back,self.d_forward,self.d_left,self.d_right)
 
-        if(distance_max <= distance_obstacle):
+        if(distance_max <= self.distance_obstacle):
             print('Everything is so close, can not move anywhere \n')
             msg_cmd_vel.linear.x=0.0
             msg_cmd_vel.angular.z=0.0
@@ -68,7 +67,7 @@ class moving:
         elif(target_surface <= 30000):
             print('See target\n')
             if(msg.x==0 and msg.y==1 and msg.z==0):
-                if(d_forward > distance_obstacle):
+                if(self.d_forward > self.distance_obstacle):
                     print("Go forward \n")
                     msg_cmd_vel.linear.x=0.1
                     msg_cmd_vel.angular.z=0.0
@@ -76,7 +75,7 @@ class moving:
                 else:
                     print('Can not go forward, it is so close \n')
             elif(msg.x==1 and msg.y==0 and msg.z==0):
-                if(d_left > distance_obstacle):
+                if(self.d_left > self.distance_obstacle):
                     print("Go to left \n")
                     msg_cmd_vel.angular.z=0.3
                     msg_cmd_vel.linear.x=0.1
@@ -84,7 +83,7 @@ class moving:
                 else:
                     print('Can not go to left, it is so close \n')
             elif(msg.x==0 and msg.y==0 and msg.z==1):
-                if(d_right > distance_obstacle):
+                if(self.d_right > self.distance_obstacle):
                     print("Go to right \n")
                     msg_cmd_vel.linear.x=0.1
                     msg_cmd_vel.angular.z=-0.7
@@ -103,6 +102,8 @@ class moving:
         print('Surface target: ', self.target_surface,'\n')
 
     def function_laser(self,msg):
+        coords = []
+        obstacles = []
         for i, theta in enumerate(np.arange(msg.angle_min, msg.angle_max, msg.angle_increment)):
             # ToDo: Remove points too close
             if msg.ranges[i] < 0.1:
@@ -111,10 +112,10 @@ class moving:
             # ToDo: Polar to Cartesian transformation
             coords.append([msg.ranges[i]*np.cos(theta), msg.ranges[i]*np.sin(theta)])
 
-        d_forward =  min(min(coords[0:45]) , min(coords[316:359]))
-        d_right = min(coords[46:135])
-        d_back = min(coords[136:225])
-        d_left = min(coords[226:315])
+        self.d_forward =  min(min(coords[0:45]) , min(coords[316:359]))
+        self.d_right = min(coords[46:135])
+        self.d_back = min(coords[136:225])
+        self.d_left = min(coords[226:315])
 
 if __name__ == '__main__':
     try:
